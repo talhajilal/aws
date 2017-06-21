ec130343b8b941b5f522c72a620ebc278e065fcf
talhajilal@gmail.com <root@ip-172-31-26-65.us-west-2.compute.internal>
2017-06-21 12:29:03 -0400

    A new changes on addd new git file

--- add_new_git_file.sh
+++ add_new_git_file.sh
@@ -1,11 +1,12 @@
-origin=$(git remote -v | grep push)
+origin=$(git remote -v | grep push | awk '{print $2}')
 echo $origin
 result=$(echo $?)
-if [$result -lt 1]; then
+if [ "$result" -eq 0 ]; then
 echo "Proceeding to commit new file"
 else 
 echo "No git hub origin define" 
 exit
+fi
 echo "Please enter your commit msg here"
 read msg
 git init .
